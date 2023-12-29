1caa973b2ab8678d9460750012590fdef3e155a0
jawad shaikh <shaikhjawad314@gmail.com>
2023-12-29 20:56:42 +0500

    fixed expired document page

--- src/app/(dashboard)/received-documents/page.tsx
+++ src/app/(dashboard)/received-documents/page.tsx
@@ -1,6 +1,7 @@
-'use client'
+"use client";
 import PageTitle from "@/components/global/PageTitle";
 import { Icons } from "@/components/global/icons";
+import Table from "@/components/ui/Table";
 import API from "@/service/api";
 import { convertDate } from "@/utils/helper";
 import { createColumnHelper } from "@tanstack/react-table";
@@ -13,59 +14,73 @@ export default function ReceivedDocumentsPage() {
   const [documents, setDocuments] = useState([]);
 
   const columns = [
-    columnHelper.accessor('id', {
+    columnHelper.accessor("id", {
       cell: (info) => info.getValue().toString(),
-      header: () => 'No',
+      header: () => "No",
     }),
-    columnHelper.accessor('title', {
-      cell: (info) => info.getValue(),
-      header: 'Name',
+    columnHelper.accessor(
+      (row) => (row.user ? `${row.user.firstName} ${row.user.lastName}` : "-"),
+      {
+        id: "name",
+        header: "Name",
+      }
+    ),
+
+    columnHelper.accessor(
+      (row) => (row.user ? `${row.user.employeeNumber}` : "-"),
+      {
+        id: "employeeNumber",
+        header: "No Employee",
+      }
+    ),
+    columnHelper.accessor((row) => (row.user ? `${row.user.idNumber}` : "-"), {
+      id: "idNumber",
+      header: "No ID",
     }),
-    columnHelper.accessor('createdAt', {
-      cell: (info) => info.getValue(),
-      header: 'No Employee',
-    }),
-    columnHelper.accessor('title', {
-      cell: (info) => info.getValue(),
-      header: 'No ID',
-    }),
-    columnHelper.accessor('title', {
-      cell: (info) => info.getValue(),
-      header: 'Title',
-    }),
-    columnHelper.accessor('title', {
-      cell: (info) => info.getValue(),
-      header: 'HR Employee',
-    }),
-    columnHelper.accessor('title', {
+    columnHelper.accessor(
+      (row) => (row?.documentRequest ? `${row.documentRequest.title}` : "-"),
+      {
+        id: "title",
+        header: "No ID",
+      }
+    ),
+
+    // columnHelper.accessor("title", {
+    //   cell: (info) => info.getValue(),
+    //   header: "HR Employee",
+    // }),
+    columnHelper.accessor("createdAt", {
       cell: (info) => convertDate(info.getValue()),
-      header: 'Upload Date',
+      header: "Upload Date",
     }),
-    columnHelper.accessor('title', {
+    columnHelper.accessor("expireDate", {
       cell: (info) => convertDate(info.getValue()),
-      header: 'Expiry Date',
+      header: "Expiry Date",
     }),
     // Add more columns as needed
   ];
 
   const fetchDocuments = async () => {
     try {
-      const { data } = await API.expiredHistory();
-      setDocuments(data.data)
+      const { data } = await API.submissionDocuments();
+      setDocuments(data.data);
+      console.log(data.data);
     } catch (error: any) {
-      toast.error(error.message)
+      toast.error(error.message);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchDocuments()
-  }, [])
-
+    fetchDocuments();
+  }, []);
 
   return (
     <>
-      <PageTitle title={"Received Documents"} icon={<Icons.received className="w-8 h-8" />} />
-
+      <PageTitle
+        title={"Received Documents"}
+        icon={<Icons.received className="w-8 h-8" />}
+      />
+      <Table data={documents} columns={columns} />
     </>
-  )
+  );
 }
